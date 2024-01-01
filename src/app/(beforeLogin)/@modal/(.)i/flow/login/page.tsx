a4a50350ff92af8b99f64675b1611cea247e56ff
ljk4268 <ljk4268@kakao.com>
2024-01-01 19:17:03 +0900

    router.replace

--- src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -1,6 +1,4 @@
-import SignupModal from '@/app/(beforeLogin)/_component/LoginModal';
+import LoginModal from '@/app/(beforeLogin)/_component/LoginModal'
 export default function Page() {
-  return (
-    <SignupModal />
-  )
+  return <LoginModal />
 }
