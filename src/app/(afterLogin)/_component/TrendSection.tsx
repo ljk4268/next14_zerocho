064787a4bbeb3a5b7584f57b6aadea17ab85c255
ljk4268 <ljk4268@kakao.com>
2024-01-02 21:05:24 +0900

    usePathname, /explore

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,16 +1,20 @@
+'use client'
+
 import style from './trendSection.module.css'
 import Trend from './Trend'
-
+import { usePathname } from 'next/navigation'
 
 export default function TrendSection() {
-return (
-  <div className={style.trendBg}>
-    <div className={style.trend}>
-      <Trend />
-      <Trend />
-      <Trend />
-      <Trend />
+  const pathname = usePathname()
+  if (pathname == '/explore') return null
+  return (
+    <div className={style.trendBg}>
+      <div className={style.trend}>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
     </div>
-  </div>
-)
+  )
 }
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,10 +1,22 @@
-import Image from 'next/image'
-import styles from '../layout.module.css'
+import style from '@/app/(afterLogin)/explore/explore.module.css'
+import SearchForm from '../_component/SearchForm'
+import Trend from '../_component/Trend'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      검색페이지
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
     </main>
   )
 }
