3a304ac109859f01dadfe595b8623b6e298e7a52
ljk4268 <ljk4268@kakao.com>
2024-01-08 22:17:38 +0900

    faker.js와 /photo/[photoId]

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,6 +1,7 @@
 import style from './post.module.css'
 import Link from 'next/link'
 import dayjs from 'dayjs'
+import { faker } from '@faker-js/faker'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/ko'
 import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons'
@@ -9,7 +10,11 @@ import PostArticle from './PostArticle'
 dayjs.locale('ko')
 dayjs.extend(relativeTime)
 
-export default function Post() {
+type Props = {
+  noImage?: boolean
+}
+
+export default function Post({noImage}: Props) {
   const target = {
     postId: 1,
     User: {
@@ -19,7 +24,11 @@ export default function Post() {
     },
     content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
+  }
+
+  if (Math.random() > 0.5) {
+    target.Images.push({ imageId: 1, link: faker.image.urlLoremFlickr() })
   }
   return (
     <PostArticle post={target}>
@@ -43,7 +52,16 @@ export default function Post() {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div className={style.postImageSection}>
+            {target.Images && target.Images.length > 0 && (
+              <Link
+                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
+                className={style.postImageSection}
+              >
+                <img src={target.Images[0]?.link} alt="" />
+              </Link>
+            )}
+          </div>
           <ActionButtons />
         </div>
       </div>
