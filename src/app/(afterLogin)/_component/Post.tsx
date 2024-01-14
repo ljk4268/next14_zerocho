385afef2186d06518809f4e218a1989e418c1c22
ljk4268 <ljk4268@kakao.com>
2024-01-14 18:40:46 +0900

    다중 이미지 구역 만들기

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,6 +6,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/ko'
 import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons'
 import PostArticle from './PostArticle'
+import PostImages from './PostImages'
 
 dayjs.locale('ko')
 dayjs.extend(relativeTime)
@@ -14,7 +15,7 @@ type Props = {
   noImage?: boolean
 }
 
-export default function Post({noImage}: Props) {
+export default function Post({ noImage }: Props) {
   const target = {
     postId: 1,
     User: {
@@ -29,6 +30,9 @@ export default function Post({noImage}: Props) {
 
   if (Math.random() > 0.5) {
     target.Images.push({ imageId: 1, link: faker.image.urlLoremFlickr() })
+    target.Images.push({ imageId: 2, link: faker.image.urlLoremFlickr() })
+    target.Images.push({ imageId: 3, link: faker.image.urlLoremFlickr() })
+    target.Images.push({ imageId: 4, link: faker.image.urlLoremFlickr() })
   }
   return (
     <PostArticle post={target}>
@@ -52,15 +56,8 @@ export default function Post({noImage}: Props) {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-            {target.Images && target.Images.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt="" />
-              </Link>
-            )}
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
